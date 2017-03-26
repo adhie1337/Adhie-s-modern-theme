055f3d9775639f14434f057c898f9930b10d2bd6
adhie1337 <adhie@inf.elte.hu>
2017-03-26 22:29:20 +0200

    Menu icon fix

---
+++ AdhiesModernTheme/AdhiesModernTheme/Scripts/AdhiesModernTheme.js
@@ -0,0 +1,9 @@
+﻿// manually wrapping the "first character"
+Array.prototype.forEach.call(document.querySelectorAll("#aside-second > div > article > nav > ul > li > a"), wrapFirstChar);
+Array.prototype.forEach.call(document.querySelectorAll("#aside-second > div > article > header > h1"), wrapFirstChar);
+
+
+function wrapFirstChar(div) {
+    let content = div.innerHTML, chars = content.charCodeAt(0) >= 55349 ? 2 : 1;
+    div.innerHTML = '<span class="icon">' + content.substring(0, chars) + "</span>" + (chars == 2 ? content.substring(chars) : content);
+}
