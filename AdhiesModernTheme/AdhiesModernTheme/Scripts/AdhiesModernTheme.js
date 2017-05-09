57d128a22cbcc9b9d7b23d779e9cf3e4e2495b87
adhie1337 <adhie@inf.elte.hu>
2017-05-09 22:17:57 +0200

    Made menu prettier.

--- AdhiesModernTheme/AdhiesModernTheme/Scripts/AdhiesModernTheme.js
+++ AdhiesModernTheme/AdhiesModernTheme/Scripts/AdhiesModernTheme.js
@@ -1,9 +1,9 @@
 ﻿// manually wrapping the "first character"
-Array.prototype.forEach.call(document.querySelectorAll("#aside-second > div > article > nav > ul > li > a"), wrapFirstChar);
-Array.prototype.forEach.call(document.querySelectorAll("#aside-second > div > article > header > h1"), wrapFirstChar);
+Array.prototype.forEach.call(
+    document.querySelectorAll(".widget-menu-widget nav > ul > li > a, .widget-menu-widget header > h1"),
+    function (div) {
+        let content = div.innerHTML, chars = content.charCodeAt(0) >= 9785 ? 2 : 1;
+        div.innerHTML = '<span class="icon">' + content.substring(0, chars) + "</span>" + (chars == 2 ? content.substring(chars) : content);
+    });
 
-
-function wrapFirstChar(div) {
-    let content = div.innerHTML, chars = content.charCodeAt(0) >= 9785 ? 2 : 1;
-    div.innerHTML = '<span class="icon">' + content.substring(0, chars) + "</span>" + (chars == 2 ? content.substring(chars) : content);
-}
+Array.prototype.forEach.call(document.getElementsByClassName("hide-menu"), element => element.classList.remove("hide-menu"));
