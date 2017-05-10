07b9538538a03bc5fbd2a53c1a08898bda686e0c
adhie1337 <adhie@inf.elte.hu>
2017-05-10 23:02:05 +0200

    Menu update: added monochrome emojis with text variant selector 15 (text representation).

--- AdhiesModernTheme/AdhiesModernTheme/Scripts/AdhiesModernTheme.js
+++ AdhiesModernTheme/AdhiesModernTheme/Scripts/AdhiesModernTheme.js
@@ -3,7 +3,7 @@ Array.prototype.forEach.call(
     document.querySelectorAll(".widget-menu-widget nav > ul > li > a, .widget-menu-widget header > h1"),
     function (div) {
         let content = div.innerHTML, chars = content.charCodeAt(0) >= 9785 ? 2 : 1;
-        div.innerHTML = '<span class="icon">' + content.substring(0, chars) + "</span>" + (chars == 2 ? content.substring(chars) : content);
+        div.innerHTML = '<span class="icon">' + content.substring(0, chars) + "&#xFE0E;</span>" + (chars == 2 ? content.substring(chars) : content).trimLeft();
     });
 
-Array.prototype.forEach.call(document.getElementsByClassName("hide-menu"), element => element.classList.remove("hide-menu"));
+Array.prototype.forEach.call(document.getElementsByClassName("hide-menu"), function (element) { element.classList.remove("hide-menu"); });
